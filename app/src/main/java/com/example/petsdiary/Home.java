8c12f47f7a80584f89d3af5224bc2b0615f5d894
Vaheelectronics <gretatepanyan768@gmail.com>
2023-03-10 19:56:41 +0400

    gaf project

---
+++ app/src/main/java/com/example/petsdiary/Home.java
@@ -0,0 +1,33 @@
+package com.example.petsdiary;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    Button switchToSecondActivity;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        switchToSecondActivity = findViewById(R.id.imageView4);
+        switchToSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
+    }
+
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, Mypet.class);
+        startActivity(switchActivityIntent);
+    }
+}
